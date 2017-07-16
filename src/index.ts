e85e5b3d1feffababd9fe45ba3ed85302ebd0ba7
Nick Johnstone <ncwjohnstone@gmail.com>
2017-07-16 16:16:15 +1200

    Hello Rogue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {run} from '@cycle/run';
-import {makeDOMDriver, div, DOMSource, VNode} from '@cycle/dom';
-import {timeDriver, TimeSource} from '@cycle/time';
-import xs, {Stream} from 'xstream';
+import { run } from '@cycle/run';
+import { makeDOMDriver, div, pre, DOMSource, VNode } from '@cycle/dom';
+import { timeDriver, TimeSource } from '@cycle/time';
+import xs, { Stream } from 'xstream';
 
 interface Sources {
   DOM: DOMSource;
@@ -9,19 +9,107 @@ interface Sources {
 }
 
 interface Sinks {
-  DOM: Stream<VNode>
+  DOM: Stream<VNode>;
 }
 
-function main (sources: Sources): Sinks {
-  console.log(sources);
-  return {
-    DOM: xs.of(div('hello world'))
+type Floor = Array<Row>;
+type Row = Array<Tile>;
+
+interface Tile {
+  ground: any;
+  space: any;
+}
+
+interface State {
+  floor: Floor;
+}
+
+function characterFor(tile: Tile): String {
+  if (tile.space && tile.space.type === 'player') {
+    return '@';
   }
+
+  if (tile.space && tile.space.type === 'wall') {
+    return '#';
+  }
+
+  if (tile.ground && tile.ground === 'ground') {
+    return '.';
+  }
+
+  return ' ';
+}
+
+function view(state: State) {
+  return div('.game', [
+    pre(
+      '.floor',
+      state.floor.map(row =>
+        div('.row', row.map(tile => div('.tile', characterFor(tile))))
+      )
+    )
+  ]);
+}
+
+const player = {
+  type: 'player',
+  health: 100,
+  time: 100
+};
+
+const wall = {
+  type: 'wall'
+};
+
+function loadMap(map: string): Floor {
+  function parseChar(character: string): Tile {
+    if (character === '@') {
+      return { ground: 'ground', space: player };
+    }
+
+    if (character === '#') {
+      return { ground: 'ground', space: wall };
+    }
+
+    if (character === '.') {
+      return { ground: 'ground', space: null };
+    }
+
+    return { ground: null, space: null };
+  }
+
+  return map.split('\n').map(line => line.split('').map(parseChar));
+}
+
+const map = `
+
+
+    ########
+    #......############
+    #..@..............#
+    #......##########.#
+    ########        #.#
+                    ###
+`;
+
+function main(sources: Sources): Sinks {
+  console.log(sources);
+
+  const initialState: State = {
+    floor: loadMap(map)
+  };
+
+
+  const state$ = xs.of(initialState);
+
+  return {
+    DOM: state$.map(view)
+  };
 }
 
 const drivers = {
   DOM: makeDOMDriver(document.body),
   Time: timeDriver
-}
+};
 
 run(main, drivers);
